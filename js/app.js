17edc1bbfd6604a7ae7732365f13f84a21e979cd
developer-suhag <developersuhag@gmail.com>
2021-09-12 23:41:43 +0600

    image issue fixed

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    console.log(product);
-    const image = product.images;
+    const image = product.image;
+    console.log(image);
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
